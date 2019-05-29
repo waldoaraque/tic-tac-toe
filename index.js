4a31684974d788b1381971751936eb51d41ceec8
waldoaraque <waldoaraque10@gmail.com>
2019-05-29 12:52:52 -0400

    implementing io.on() in our backend

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require('http').Server();
+const io = require('socket.io')(server);
+const port = require('./config').SERVER_PORT;
+
+io.on('connection', socket => {
+  socket.on('register', user => {
+    console.info(`User registered: ${user.name}`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server runing at port ${port}`);
+})
