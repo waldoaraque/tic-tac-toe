c7093ddd19be71a5ee90ca80350e2d316fdfaf28
waldoaraque <waldoaraque10@gmail.com>
2019-05-30 16:24:57 -0400

    adding the backend to the project

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const server = require('http').Server();
 const io = require('socket.io')(server);
 const port = require('./config').SERVER_PORT;
 
+require('./backend/connect')(io);
+
 io.on('connection', socket => {
   socket.on('register', user => {
     console.info(`User registered: ${user.name}`);
@@ -10,4 +12,4 @@ io.on('connection', socket => {
 
 server.listen(port, () => {
   console.log(`Server runing at port ${port}`);
-})
+});
